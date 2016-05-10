bd801bd507811df00c6871570d55483ac4e6ba6a
Arun Merum <amerum@cisco.com>
2016-05-10 10:34:49 +0530

    Committed the resource to list the running JVMS

--- src/main/java/com/im/services/JVMService.java
+++ src/main/java/com/im/services/JVMService.java
@@ -3,10 +3,12 @@ package com.im.services;
 import com.im.beans.JVM;
 import com.sun.tools.attach.VirtualMachine;
 import com.sun.tools.attach.VirtualMachineDescriptor;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class JVMService {
 
     public List<JVM> getAllJVMs() {
